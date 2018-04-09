3665b75600084d9e052c1ac378ec2a034122822f
rockey <446175480@qq.com>
2018-04-09 20:41:50 +0800

    fix

--- controller/admin/user.js
+++ controller/admin/user.js
@@ -1,19 +1,4 @@
 const ListService = require('../../service/admin/user/list')
-  // 从数据库中读取所有用户数据
-  /*
-   *   sort() 可对字段指定排序 传值 -1降序 1 升序
-   *   对数据进行分页
-   *   limit(number) 限制从数据库中取出条数
-   *   skip(number) 忽略数据的条数
-   *
-   *   eg:每页显示2条
-   *   第一页：1-2 skip 0  -> 当前页 -1 * limit
-   *   第二页：3-4 skip 2 ->
-   *   ...
-   *
-   * */
-  // let page = 1
-  // console.log(req.query.page)
 module.exports.list = async (ctx) => {
   const opts = ctx.query
   let s = new ListService(opts, ctx)
@@ -22,7 +7,3 @@ module.exports.list = async (ctx) => {
   code = JSON.parse(code)
   ctx.body = await ctx.render('admin/user_index', code)
 }
-// function list(req, res, next) {
-
-
-// }
